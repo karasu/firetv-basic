604f702bcf02aaf4c09d86be01ab35d7234ea761
Gustau Castells <gustaucastells@gmail.com>
2018-12-24 22:23:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,8 @@ sudo apt-get install adb
 sudo apt-get install python3-flask gunicorn3
 
 install firetv-rest-server.py /usr/local/bin/firetv-rest-server.py
+
+echo "To run the REST server:"
+echo "cd /usr/local/bin"
+echo "gunicorn -w 4 -b 127.0.0.1:4000 firetv_rest_server:app &"
+
